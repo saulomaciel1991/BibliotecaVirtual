6973bac659c2d3da7fcaf080127dc82bb287812c
saulo <saulomcm@gmail.com>
2018-05-09 19:35:58 +0000

    App de Biblioteca Virtual usando Angular/Clarity

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { ClarityModule } from '@clr/angular';
+import { AppComponent } from './app.component';
+import { ROUTING } from "./app.routing";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { BookComponent } from './book/book.component';
+import { LibraryComponent } from './library/library.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        AboutComponent,
+        HomeComponent,
+        BookComponent,
+        LibraryComponent
+    ],
+    imports: [
+        BrowserAnimationsModule,
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        ClarityModule,
+        ROUTING
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
